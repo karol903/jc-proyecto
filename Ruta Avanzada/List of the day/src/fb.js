5f064bd34c39e674c7488b5444931363deb81c10
karol cardona <kfcardonaaponte14@gmail.com>
2023-12-04 09:49:46 -0500

    5

---
+++ Ruta Avanzada/List of the day/src/fb.js
@@ -0,0 +1,8 @@
+export const app = firebase.initializeApp({
+    "projectId": "proyecto-jovenes",
+    "appId": "1:849062274813:web:b3cfe3840d39fea37c8094",
+    "storageBucket": "proyecto-jovenes.appspot.com",
+    "apiKey": "AIzaSyCp7AxCI3N6wBBck2ey5a_VTKN1kyP7uK8",
+    "authDomain": "proyecto-jovenes.firebaseapp.com",
+    "messagingSenderId": "849062274813"
+  });
